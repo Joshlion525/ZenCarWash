58c143168cdaddef3ef0ae39f4a4a3ac80d2de62
Joshlion525 <78796082+Joshlion525@users.noreply.github.com>
2024-07-30 17:34:03 +0100

    ..

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,11 +5,55 @@ import ProfilePhoto from "../assets/man-438081_960_720.svg";
 import { Link as RouterLink } from "react-router-dom";
 import { IoIosSearch } from "react-icons/io";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { RxClock } from "react-icons/rx";
+import { PiListChecks } from "react-icons/pi";
 
 const Dashboard = () => {
+	const Bookings = [
+		{
+			id: 1,
+			name: "Adebanjo Joshua",
+			vechicleRegNo: "122ewc35",
+			date: "31 July 2024",
+			Time: "3:00pm",
+			Amount: "$100",
+		},
+		{
+			id: 2,
+			name: "Adebanjo Joshua",
+			vechicleRegNo: "122ewc35",
+			date: "31 July 2024",
+			Time: "3:00pm",
+			Amount: "$100",
+		},
+		{
+			id: 3,
+			name: "Adebanjo Joshua",
+			vechicleRegNo: "122ewc35",
+			date: "31 July 2024",
+			Time: "3:00pm",
+			Amount: "$100",
+		},
+		{
+			id: 4,
+			name: "Adebanjo Joshua",
+			vechicleRegNo: "122ewc35",
+			date: "31 July 2024",
+			Time: "3:00pm",
+			Amount: "$100",
+		},
+		{
+			id: 5,
+			name: "Adebanjo Joshua",
+			vechicleRegNo: "122ewc35",
+			date: "31 July 2024",
+			Time: "3:00pm",
+			Amount: "$100",
+		},
+	];
 	return (
-		<div className="font-Poppins px-5 md:px-10">
-			<div className="flex items-center justify-between gap-4 py-2 md:py-5">
+		<div className="font-Poppins md:pl-10">
+			<div className="flex items-center justify-between py-2 md:py-5 px-3 md:px-0 md:pr-10">
 				<div className="flex items-center gap-4 md:gap-16">
 					<RouterLink to="/">
 						<img
@@ -44,7 +88,7 @@ const Dashboard = () => {
 							className="w-full"
 						/>
 					</div>
-					<div className="flex flex-col items-center md:items-start">
+					<div className="flex flex-col items-left md:items-start">
 						<h1 className="font-medium md:font-bold text-sm md:text-base">
 							Moni Roy
 						</h1>
@@ -55,8 +99,8 @@ const Dashboard = () => {
 					</div>
 				</div>
 			</div>
-			<div className="block md:hidden mt-1">
-				<div className="flex items-center gap-2">
+			<div className="block md:hidden mt-1 px-3 md:px-0">
+				<div className="flex items-center gap-2 pb-5">
 					<div className="flex items-center border gap-2 rounded-full px-4 bg-[#F5F6FA] flex-1">
 						<IoIosSearch className="text-2xl text-gray-400" />
 						<input
@@ -71,6 +115,104 @@ const Dashboard = () => {
 							alt="Camera Icon"
 							className="w-full"
 						/>
+					</div>
+				</div>
+			</div>
+			<div className="flex">
+				<div className="border-r min-h-screen pt-8 pr-8 hidden md:block">
+					<div className="flex items-center gap-5 w-52 p-4 rounded-md cursor-pointer text-buttonBg hover:bg-buttonBg hover:text-white mb-5">
+						<div className="hover:text-white">
+							<RxClock className="text-xl" />
+						</div>
+						<p className="font-medium hover:text-white">
+							Dashboard
+						</p>
+					</div>
+					<div className="flex items-center gap-5 w-52 p-4 rounded-md text-buttonBg cursor-pointer hover:bg-buttonBg hover:text-white">
+						<div>
+							<PiListChecks className="text-xl" />
+						</div>
+						<p className="font-medium hover:text-white">Bookings</p>
+					</div>
+				</div>
+				<div className="bg-[#F5F6FA] flex-1 p-3 md:p-8 w-full">
+					<div className="block md:hidden mb-10 w-full">
+						<div className="flex justify-between gap-5">
+							<div className="flex items-center gap-2 md:gap-5 w-52 p-4 rounded-md cursor-pointer text-buttonBg bg-white hover:bg-buttonBg hover:text-white">
+								<div className="hover:text-white">
+									<RxClock className="text-xl" />
+								</div>
+								<p className="font-medium hover:text-white">
+									Dashboard
+								</p>
+							</div>
+							<div className="flex items-center gap-2 md:gap-5 w-52 p-4 rounded-md text-buttonBg cursor-pointer bg-white hover:bg-buttonBg hover:text-white">
+								<div>
+									<PiListChecks className="text-xl" />
+								</div>
+								<p className="font-medium hover:text-white">
+									Bookings
+								</p>
+							</div>
+						</div>
+					</div>
+					<h1 className="font-semibold text-4xl mb-10">Bookings</h1>
+					<div className="border rounded-2xl">
+						<div className="overflow-x-auto">
+							<table className="min-w-full bg-white rounded-2xl">
+								<thead className="font-semibold text-left border-b">
+									<tr>
+										<th className="py-5 px-4 md:px-10 font-semibold">
+											ID
+										</th>
+										<th className="py-5 px-4 md:px-10 font-semibold">
+											NAME
+										</th>
+										<th className="py-5 px-4 md:px-10 font-semibold">
+											VEHICLE REGISTRATION NO
+										</th>
+										<th className="py-5 px-4 md:px-10 font-semibold">
+											Date
+										</th>
+										<th className="py-5 px-4 md:px-10 font-semibold">
+											Time
+										</th>
+										<th className="py-5 px-4 md:px-10 font-semibold text-center">
+											Amount
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									{Bookings.map((Booking) => (
+										<tr
+											className="border-b"
+											key={Booking.id}
+										>
+											<td className="py-5 px-4 md:px-10">
+												{Booking.id}
+											</td>
+											<td className="py-5 px-4 md:px-10">
+												{Booking.name}
+											</td>
+											<td className="py-5 px-4 md:px-10">
+												{Booking.vechicleRegNo}
+											</td>
+											<td className="py-5 px-4 md:px-10">
+												{Booking.date}
+											</td>
+											<td className="py-5 px-4 md:px-10">
+												{Booking.Time}
+											</td>
+											<td className="py-5 px-4 md:px-10 text-center">
+												<p className="border py-1 text-buttonBg font-bold bg-[#86bc87] rounded-md">
+													{Booking.Amount}
+												</p>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>

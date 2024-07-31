5e117d404688993da6052a6df07066e9a9b933c6
Joshlion525 <78796082+Joshlion525@users.noreply.github.com>
2024-07-31 12:51:31 +0100

    ..

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/Logo.svg";
 import Camera from "../assets/Cameraicon.svg";
 import ProfilePhoto from "../assets/man-438081_960_720.svg";
@@ -9,6 +9,9 @@ import { RxClock } from "react-icons/rx";
 import { PiListChecks } from "react-icons/pi";
 
 const Dashboard = () => {
+	const [selectedBooking, setSelectedBooking] = useState(null);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const Bookings = [
 		{
 			id: 1,
@@ -17,6 +20,10 @@ const Dashboard = () => {
 			date: "31 July 2024",
 			Time: "3:00pm",
 			Amount: "$100",
+			email: "joshlion525@gmail.com",
+			vechicleType: "Car",
+			service: "Handwash",
+			description: "Don't wash the engine",
 		},
 		{
 			id: 2,
@@ -25,6 +32,10 @@ const Dashboard = () => {
 			date: "31 July 2024",
 			Time: "3:00pm",
 			Amount: "$100",
+			email: "joshlion525@gmail.com",
+			vechicleType: "Car",
+			service: "Handwash",
+			description: "Don't wash the engine",
 		},
 		{
 			id: 3,
@@ -33,6 +44,10 @@ const Dashboard = () => {
 			date: "31 July 2024",
 			Time: "3:00pm",
 			Amount: "$100",
+			email: "joshlion525@gmail.com",
+			vechicleType: "Car",
+			service: "Handwash",
+			description: "Don't wash the engine",
 		},
 		{
 			id: 4,
@@ -41,6 +56,10 @@ const Dashboard = () => {
 			date: "31 July 2024",
 			Time: "3:00pm",
 			Amount: "$100",
+			email: "joshlion525@gmail.com",
+			vechicleType: "Car",
+			service: "Handwash",
+			description: "Don't wash the engine",
 		},
 		{
 			id: 5,
@@ -49,11 +68,26 @@ const Dashboard = () => {
 			date: "31 July 2024",
 			Time: "3:00pm",
 			Amount: "$100",
+			email: "joshlion525@gmail.com",
+			vechicleType: "Car",
+			service: "Handwash",
+			description: "Don't wash the engine",
 		},
 	];
+
+	const handleRowClick = (booking) => {
+		setSelectedBooking(booking);
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+		setSelectedBooking(null);
+	};
+
 	return (
-		<div className="font-Poppins md:pl-10">
-			<div className="flex items-center justify-between py-2 md:py-5 px-3 md:px-0 md:pr-10">
+		<div className="font-Poppins md:px-10">
+			<div className="flex items-center justify-between gap-4 py-2 md:py-5 px-3 md:px-0">
 				<div className="flex items-center gap-4 md:gap-16">
 					<RouterLink to="/">
 						<img
@@ -185,8 +219,11 @@ const Dashboard = () => {
 								<tbody>
 									{Bookings.map((Booking) => (
 										<tr
-											className="border-b"
+											className="border-b cursor-pointer"
 											key={Booking.id}
+											onClick={() =>
+												handleRowClick(Booking)
+											}
 										>
 											<td className="py-5 px-4 md:px-10">
 												{Booking.id}
@@ -216,6 +253,75 @@ const Dashboard = () => {
 					</div>
 				</div>
 			</div>
+
+			{isModalOpen && selectedBooking && (
+				<div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+					<div className="bg-white p-6 rounded-lg">
+						<h2 className="text-2xl font-medium mb-4 text-buttonBg italic">
+							Booking Summary
+						</h2>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">Name -</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.name}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">
+								Email address -
+							</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.email}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">
+								Vechicle type -
+							</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.vechicleType}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">
+								Vechicle registration number -
+							</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.vechicleRegNo}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">Service -</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.service}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">
+								Date & time -
+							</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.date} - {selectedBooking.Time}
+							</p>
+						</div>
+						<div className="flex flex-col md:flex-row md:items-center md:gap-3">
+							<h1 className="font-medium text-xl">
+								Description -
+							</h1>
+							<p className="text-lg text-buttonBg font-light italic">
+								{selectedBooking.description}
+							</p>
+						</div>
+
+						<button
+							onClick={closeModal}
+							className="mt-4 py-2 px-4 bg-buttonBg text-white rounded"
+						>
+							Close
+						</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
